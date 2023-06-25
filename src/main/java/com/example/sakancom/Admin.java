b14e312fc04ea60d0a1b25e00b212faab0abac23
yazanmansour <yazan.mansour2003@gmail.com>
2023-06-25 20:11:40 +0300

    simple login

---
+++ src/main/java/com/example/sakancom/Admin.java
@@ -0,0 +1,16 @@
+package com.example.sakancom;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Admin {
+
+
+
+}
---
+++ src/main/java/com/example/sakancom/TenantController.java
@@ -0,0 +1,14 @@
+package com.example.sakancom;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class TenantController {
+
+}
